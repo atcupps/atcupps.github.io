b8db6899252756b82bd7ce30eaead6009f7940b8
atcupps <91998663+atcupps@users.noreply.github.com>
2023-01-03 15:09:14 -0500

    Added topnav

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ var year = d.getFullYear() - 2004;
 var month = d.getMonth() + 1;
 var day = d.getDay() + 1;
 document.getElementById("version").innerHTML = "" + year + "." + month + "." + day;
+
+var curDisplay = "andrewcupps";
+document.getElementById("andrewcuppsbutton").onclick = function() {
+    if (curDisplay != "andrewcupps") {
+        curDisplay = "andrewcupps";
+        document.getElementById("andrewcupps").style.visibility = "visible";
+    }
+
+    //document.getElementById("andrewcupps").style.visibility = "hidden";
+}
