8cbf305c8e4b5b9684c41bf910ec26b2e9ff01ef
atcupps <91998663+atcupps@users.noreply.github.com>
2023-01-03 12:35:25 -0500

    Moved script from inline to app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 'use strict'
 
+//Writing version
+const d = new Date();
+var year = d.getFullYear() - 2004;
+var month = d.getMonth() + 1;
+var day = d.getDay() + 1;
+document.getElementById("version").innerHTML = "" + year + "." + month + "." + day;
