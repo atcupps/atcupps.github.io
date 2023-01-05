f582971538303138d1a3203c3c290e70344a53b9
atcupps <91998663+atcupps@users.noreply.github.com>
2023-01-05 12:50:41 -0500

    Added button/link hover transitions

--- app.js
+++ app.js
@@ -36,37 +36,57 @@ document.getElementById("runBtn").onclick = function() {
  * Several functions to animate the appearance of text in the console section of the page
  */
 var speed = 250;
+var shouldPrintFirstLineTwo = false;
+var shouldPrintSecond = false;
+var shouldPrintThird = false;
+var shouldPrintFourth = false;
 var shouldPrintLast = false;
 
 function writeConsole() {
+    shouldPrintSecond = false;
+    shouldPrintFirstLineTwo = false;
+    shouldPrintThird = false;
+    shouldPrintFourth = false;
     shouldPrintLast = false;
     setTimeout(writeConsole1, speed);
 }
 
 function writeConsole1() {
     document.getElementById("console1").innerHTML = "> Hello, my name is Andrew Cupps, and I am an undergraduate computer science and astronomy student at the University of Maryland!";
+    shouldPrintFirstLineTwo = true;
     setTimeout(writeConsole1ln2, speed);
 }
 
 function writeConsole1ln2() {
-    document.getElementById("console1ln2").innerHTML = "Feel free to explore my website, or contact me at the email listed below. ";
-    setTimeout(writeConsole2, speed);
+    if (shouldPrintFirstLineTwo) {
+        document.getElementById("console1ln2").innerHTML = "Feel free to explore my website, or contact me at the email listed below. ";
+        shouldPrintSecond = true;
+        setTimeout(writeConsole2, speed);
+    }
 }
 
 function writeConsole2() {
-    document.getElementById("console2").innerHTML = "> My email: at.cupps@gmail.com";
-    setTimeout(writeConsole3, speed);
+    if (shouldPrintSecond) {
+        document.getElementById("console2").innerHTML = "> My email: at.cupps@gmail.com";
+        shouldPrintThird = true;
+        setTimeout(writeConsole3, speed);
+    }
 }
 
 function writeConsole3() {
-    document.getElementById("console3").innerHTML = "> Languages I write in: [Java, R, Matlab, JavaScript, HTML/CSS, Python, C++]";
-    setTimeout(writeConsole4, speed);
+    if (shouldPrintThird) {
+        document.getElementById("console3").innerHTML = "> Languages I write in: [Java, R, Matlab, JavaScript, HTML/CSS, Python, C++]";
+        shouldPrintFourth = true;
+        setTimeout(writeConsole4, speed);
+    }
 }
 
 function writeConsole4() {
-    document.getElementById("console4").innerHTML = "> Software/Tools I use: [Eclipse, JUnit, RStudio, LaTeX, Git, GitHub, Matlab, VS Code, Blender]";
-    shouldPrintLast = true;
-    setTimeout(writeConsole5, speed);
+    if (shouldPrintFourth) {
+        document.getElementById("console4").innerHTML = "> Software/Tools I use: [Eclipse, JUnit, RStudio, LaTeX, Git, GitHub, Matlab, VS Code, Blender]";
+        shouldPrintLast = true;
+        setTimeout(writeConsole5, speed);
+    }
 }
 
 var hasVertLine = false;
