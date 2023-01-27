09897071e834474661ac1d18caa601527e9baff2
atcupps <91998663+atcupps@users.noreply.github.com>
2023-01-27 13:39:04 -0500

    Added more page layout

--- index.js
+++ index.js
@@ -1,94 +1,11 @@
 'use strict'
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const itersPerLetter = 3;
+function offsetAnchor() {
+    if(location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY - 100);
+    }
+}
 
-//Menu Hover effects
+window.addEventListener("hashchange", offsetAnchor);
 
-// document.querySelector("#about").onmouseover = event => {
-//     var iters = 0;
-
-//     const interval = setInterval(() => {
-//         event.target.innerText = event.target.innerText.split("")
-//         .map((letter, index) => {
-//             if (index < iters) {
-//                 return event.target.dataset.value[index];
-//             }
-
-//             return letters[Math.floor(Math.random() * 26)];
-//         })
-//         .join("");
-
-//         if (iters >= event.target.dataset.value.length) {
-//             clearInterval(interval);
-//         }
-
-//         iters += 1 / itersPerLetter;
-//     }, 30);
-// }
-
-// document.querySelector("#portfolio").onmouseover = event => {
-//     var iters = 0;
-
-//     const interval = setInterval(() => {
-//         event.target.innerText = event.target.innerText.split("")
-//         .map((letter, index) => {
-//             if (index < iters) {
-//                 return event.target.dataset.value[index];
-//             }
-
-//             return letters[Math.floor(Math.random() * 26)];
-//         })
-//         .join("");
-
-//         if (iters >= event.target.dataset.value.length) {
-//             clearInterval(interval);
-//         }
-
-//         iters += 1 / itersPerLetter;
-//     }, 30);
-// }
-
-// document.querySelector("#resume").onmouseover = event => {
-//     var iters = 0;
-
-//     const interval = setInterval(() => {
-//         event.target.innerText = event.target.innerText.split("")
-//         .map((letter, index) => {
-//             if (index < iters) {
-//                 return event.target.dataset.value[index];
-//             }
-
-//             return letters[Math.floor(Math.random() * 26)];
-//         })
-//         .join("");
-
-//         if (iters >= event.target.dataset.value.length) {
-//             clearInterval(interval);
-//         }
-
-//         iters += 1 / itersPerLetter;
-//     }, 30);
-// }
-
-// document.querySelector("#contact").onmouseover = event => {
-//     var iters = 0;
-
-//     const interval = setInterval(() => {
-//         event.target.innerText = event.target.innerText.split("")
-//         .map((letter, index) => {
-//             if (index < iters) {
-//                 return event.target.dataset.value[index];
-//             }
-
-//             return letters[Math.floor(Math.random() * 26)];
-//         })
-//         .join("");
-
-//         if (iters >= event.target.dataset.value.length) {
-//             clearInterval(interval);
-//         }
-
-//         iters += 1 / itersPerLetter;
-//     }, 30);
-// }
+window.setTimeout(offsetAnchor, 1); //arbitrary
